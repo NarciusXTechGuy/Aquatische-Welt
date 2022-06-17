c35d568e32bd5c54dccb8e83a60a0063e000a656
kefi <tenwinjames.98@gmail.com>
2022-06-17 11:52:51 +0530

    update 17/06/2022

--- Aquatische-Welt/src/app/app.module.ts
+++ Aquatische-Welt/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './sharepage/navbar/navbar.component';
+import { FooterComponent } from './sharepage/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { MenuComponent } from './pages/menu/menu.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { AboutComponent } from './pages/about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent,
+    MenuComponent,
+    ContactComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
